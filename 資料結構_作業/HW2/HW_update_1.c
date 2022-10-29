1ede18cd8be952dd8760fc03165ba2b975faddcc
diego840 <yaught843294@gmail.com>
2022-10-29 20:26:53 +0800

    add sorting 1

--- 資料結構_作業/HW2/HW_update_1.c
+++ 資料結構_作業/HW2/HW_update_1.c
@@ -3,14 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 #define max 1050
+//  char
+char shape[max]; //存每行input的share (字串型態)
+char *token; //strtok中的token
+//  int
 int resource_y,resource_x,req;
-char shape[max];
-int i_shape[max][max];
-int shape_num[max];
-int original_ID[max];
-int sort_tmp[max];
-int sort_src = 0;
-char *token;
+//存一條ID中各種型態的shape !use function:[input,Row_sorting,swap,Column_sorting,Print]
+int candidate_shape[max][max]; // [Y][X] 
+int shape_num[max]; //存一條ID中有多少組的Shape !use function:[input,Row_sorting,swap,Print]
+int original_ID[max]; //存sorting前的ID !use function:[input,swap,Print]
+int tmp_list[max];  //tmp list !use function:[Row_sorting,swap]
+//  輸入
 void input(){
     scanf("%d %d %d",&resource_y,&resource_x,&req);
     for (int i = 0; i < req; i++){
@@ -19,46 +22,48 @@ void input(){
         token = strtok(shape,"x ");
         while (token != NULL){
             if (req_index > 0){
-                i_shape[i][req_index-1] = atoi(token);
+                candidate_shape[i][req_index-1] = atoi(token);
             }
             req_index++;
             token = strtok(NULL,"x ");
         }
         shape_num[i] = req_index - 1;
         original_ID[i] = i;
-        i_shape[i][req_index-1] = -1;
+        candidate_shape[i][req_index-1] = -1;
     }
 }
-int cmpmax(const void * a, const void * b){
+//  同行share排序,row sorting
+int cmpmin(const void * a, const void * b){
     return( *(int*)a - *(int*)b );    
 }
-int cmpmin(const void * a,const void * b){
+int cmpmax(const void * a,const void * b){
     return( *(int*)b - *(int*)a );
 }
 void Row_Sorting(int x){
     for (int i = 0; i < req; i++){
         if (shape_num[i] > 2){
-            for (int j = 0,jj = 0; i_shape[i][j] != -1; j = j+2,jj++){
-                sort_tmp[jj] = i_shape[i][j];
+            for (int j = 0,jj = 0; candidate_shape[i][j] != -1; j = j+2,jj++){
+                tmp_list[jj] = candidate_shape[i][j];
             }
             if (x == 0){
-                qsort(sort_tmp,(shape_num[i]/2),sizeof(int),cmpmax);
+                qsort(tmp_list,(shape_num[i]/2),sizeof(int),cmpmin);
             }else{
-                qsort(sort_tmp,(shape_num[i]/2),sizeof(int),cmpmin);
+                qsort(tmp_list,(shape_num[i]/2),sizeof(int),cmpmax);
             }
-            for (int k = 0,kk = 0; i_shape[i][k] != -1;k = k+2,kk++){
-                i_shape[i][k] = sort_tmp[kk];
-                i_shape[i][k+1] = resource_y/sort_tmp[kk];
+            for (int k = 0,kk = 0; candidate_shape[i][k] != -1;k = k+2,kk++){
+                candidate_shape[i][k] = tmp_list[kk];
+                candidate_shape[i][k+1] = resource_y/tmp_list[kk];
             }
         }      
     }
 }
+//  不同行shape排序,column sorting
 void swap(int x, int y){
     int swap_tmp;
-    memset(sort_tmp,0,sizeof(sort_tmp));
-    memcpy(sort_tmp,i_shape[x],sizeof(i_shape[x]));
-    memcpy(i_shape[x],i_shape[y],sizeof(i_shape[x]));
-    memcpy(i_shape[y],sort_tmp,sizeof(i_shape[x]));
+    memset(tmp_list,0,sizeof(tmp_list));
+    memcpy(tmp_list,candidate_shape[x],sizeof(candidate_shape[x]));
+    memcpy(candidate_shape[x],candidate_shape[y],sizeof(candidate_shape[x]));
+    memcpy(candidate_shape[y],tmp_list,sizeof(candidate_shape[x]));
     swap_tmp = shape_num[x];
     shape_num[x] = shape_num[y];
     shape_num[y] = swap_tmp;
@@ -66,35 +71,16 @@ void swap(int x, int y){
     original_ID[x] = original_ID[y];
     original_ID[y] = swap_tmp;
 }
-/*void Column_QuickSort(int left,int right){
-    if (left < right){
-        int formula = (left+right)/2;
-        int pivot = i_shape[formula][0];
-        int i = left-1;
-        int j = right+1;
-
-        while (1){
-            while(i_shape[++i][0] > pivot);
-            while(i_shape[--j][0] < pivot);
-            if (i >= j){
-                break;
-            }
-            swap(i,j);
-        }
-        Column_QuickSort(left,i-1);
-        Column_QuickSort(j+1,right);
-    }
-}*/
 void Column_Sorting(int left,int right){
     if (left < right){
         int formula = (left+right)/2;
-        int pivot = i_shape[formula][0];
+        int pivot = candidate_shape[formula][0];
         int i = left-1;
         int j = right+1;
 
         while (1){
-            while(i_shape[++i][0] > pivot);
-            while(i_shape[--j][0] < pivot);
+            while(candidate_shape[++i][0] > pivot);
+            while(candidate_shape[--j][0] < pivot);
             if (i >= j){
                 break;
             }
@@ -104,34 +90,17 @@ void Column_Sorting(int left,int right){
         Column_Sorting(j+1,right);
     }
 }
-void Column_Class_Sorting(int left, int right){
-    if (left < right){
-        int formula = (left+right)/2;
-        int pivot = i_shape[formula][0];
-        int i = left-1;
-        int j = right+1;
-
-        while (1){
-            while(i_shape[++i][0] < pivot);
-            while(i_shape[--j][0] > pivot);
-            if (i >= j ){
-                break;
-            }
-            swap(i,j);
-        }
-        Column_Class_Sorting(left,i-1);
-        Column_Class_Sorting(j+1,right);
-    }
-}
+//  打印
 void Print(){
     for (int i = 0; i < req; i++){
-        for (int j = 0; i_shape[i][j] != -1; j++){
-            printf("%d ",i_shape[i][j]);   
+        for (int j = 0; candidate_shape[i][j] != -1; j++){
+            printf("%d ",candidate_shape[i][j]);   
         }
         printf("\nshape_num: %d original_ID:%d",shape_num[i],original_ID[i]);
         printf("\n");
     }
 }
+//  main
 int main(){
     input();
     Row_Sorting(1);
